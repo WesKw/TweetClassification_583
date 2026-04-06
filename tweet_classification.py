8cae79089500c8ed3e8423465f0802f94621824d
WesKw <kwiecinskiwesley@gmail.com>
2026-04-06 15:52:01 -0500

    Small adjustments

--- tweet_classification.py
+++ tweet_classification.py
@@ -1,10 +1,28 @@
 import pandas as pd
 import scipy
 
+from time import time
 from argparse import ArgumentParser
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def load_tweets(input: str) -> list:
+def calculate_precision():
+    ...
+
+
+def calculate_recall():
+    ...
+
+
+def calculate_fscore():
+    ...
+
+
+def calculate_accuracy():
+    ...
+
+
+def load_tweet_data(input: str) -> dict:
     """
     Loads tweets into a pandas dataframe. Input is assumed to be an xlsx file.
     """
@@ -12,22 +30,31 @@ def load_tweets(input: str) -> list:
     romney_df = pd.DataFrame()
     if ".xlsx" in input:
         data = pd.read_excel(input, sheet_name=None, header=0)
-        # print(data)
         obama_df = data["Obama"]
         romney_df = data["Romney"]
 
-    return [obama_df, romney_df]
+    return {"Obama": {"df": obama_df, "classifier": None}, "Romney": {"df": romney_df, "classifier": None}}
+
+
+def build_classifier(df: pd.DataFrame):
+    start = time()
+    text_vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, min_df=5, stop_words="english")
+
+    tweets = df["Anotated Tweet"]
+    classes = df["Class"]
+
+    return None
 
 
 def clean_data(data: pd.DataFrame) -> pd.DataFrame:
     df = data.drop(index=0)
-    df = df.set_axis(labels=['None', 'date', 'time', 'Anotated Tweet', 'Class', 'Yourclass'], axis=1)
-    # print(df.columns)
-    df = df.drop(labels=['None', 'Yourclass'], axis=1)
-    df = df[(df["Class"] != 2) & (df["Class"] != "!!!!") & (df["Class"] != "irrevelant")] # remove any classes that are not 0 1 or -1
-    # data = data[(data["Unnamed: 4"] != 2)]
-    # data = data.drop(labels="Unnamed: 5")
-    
+    df = df.set_axis(labels=['None', 'date', 'time', 'Anotated Tweet', 'Class', 'Yourclass'], axis=1) # fix labels
+    df = df.drop(labels=['None', 'Yourclass'], axis=1) # drop irrelevant labels
+    df = df[(df["Class"] != 2) & (df["Class"] != "!!!!") & (df["Class"] != "irrevelant")] # remove any classes that are not 0, 1, -1
+
+    # todo:: fix datetimes?
+
+    # return cleaned df
     return df
 
 
@@ -38,8 +65,14 @@ if __name__ == "__main__":
     args.add_argument("-o", "--output", help="The file to save the output to.")
     opts = args.parse_args()
 
-    data = load_tweets(opts.training)
+    training_dfs = load_tweet_data(opts.training)
 
-    for df in data:
-        df = clean_data(df)
-        print(df)
+    for key,df_data in training_dfs.items():
+        df_data["df"] = clean_data(df_data["df"])
+
+        # build the classifier
+        classifier = build_classifier(df_data["df"])
+
+
+    # load the test data after we train our model
+    # test_dfs = load_tweet_data(opts.test)
