ac937c642af69c6989943e15f9a3ef69538048f7
WesKw <kwiecinskiwesley@gmail.com>
2026-04-09 16:08:19 -0500

    Initial classifiers

--- tweet_classification.py
+++ tweet_classification.py
@@ -5,23 +5,14 @@ from time import time
 from argparse import ArgumentParser
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import RidgeClassifier
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
 # from sklearn.pipeline import Pipeline
 
 
-def calculate_precision():
-    ...
-
-
-def calculate_recall():
-    ...
-
-
-def calculate_fscore():
-    ...
-
-
-def calculate_accuracy():
-    ...
+VALID_CLASSES = ['-1', '0', '1']
 
 
 def load_tweet_data(input: str) -> dict:
@@ -48,25 +39,34 @@ def build_basic_classifier(df: pd.DataFrame, text_vectorizer):
     x_vector = text_vectorizer.fit_transform(tweets)
     clf = RidgeClassifier(solver="sparse_cg")
     clf.fit(x_vector, classes)
-    print(time() - start)
+    print(f"Time spent building classifier: {time() - start}s")
 
     return clf
 
 
-def test_classifier(clf, test_df, vectorizer):
-    test_tweets = list(clean_data(test_data["Obama"])["Anotated Tweet"])
-    transformed_tweets = vectorizer.transform(test_tweets)
-    prediction = obama_classifier.predict(transformed_tweets)
-    print(prediction)
+def test_classifier(classifier, tweets, correct_classes, vectorizer, classifier_name=""):
+    transformed_tweets = vectorizer.transform(tweets)
+    prediction = classifier.predict(transformed_tweets)
+    precision = precision_score(correct_classes, prediction, labels=VALID_CLASSES, average=None)
+    recall = recall_score(correct_classes, prediction, labels=VALID_CLASSES, average=None)
+    f1score = f1_score(correct_classes, prediction, labels=VALID_CLASSES, average=None)
+    accuracy = accuracy_score(correct_classes, prediction)
+
+    # print(prediction)
+    print(f"Evaluation for {classifier_name}:")
+    print(f"    Accuracy: {accuracy}")
+    print(f"    Precision: [ Negative: {precision[0]} | Mixed: {precision[1]} | Positive: {precision[2]} ]")
+    print(f"    Recall: [ Negative: {recall[0]} | Mixed: {recall[1]} | Positive: {recall[2]} ]")
+    print(f"    F1 Score: [ Negative: {f1score[0]} | Mixed: {f1score[1]} | Positive: {f1score[2]} ]")
 
 
 def clean_data(data: pd.DataFrame) -> pd.DataFrame:
     df = data.drop(index=0)
     df = df.set_axis(labels=['None', 'date', 'time', 'Anotated Tweet', 'Class', 'Yourclass'], axis=1) # fix labels
     df = df.drop(labels=['None', 'Yourclass'], axis=1) # drop irrelevant labels
-    df = df[(df["Class"] != 2) & (df["Class"] != "!!!!") & (df["Class"] != "irrevelant")] # remove any classes that are not 0, 1, -1
-    df = df.dropna()
     df["Class"] = df["Class"].astype("string")
+    df = df[df["Class"].isin(VALID_CLASSES)] # remove any rows that have bad class labels
+    df = df.dropna()
 
     # todo:: fix datetimes?
 
@@ -82,32 +82,32 @@ if __name__ == "__main__":
     opts = args.parse_args()
 
     training_data = load_tweet_data(opts.training)
-    obama_data = clean_data(training_data["Obama"])
-    romney_data = clean_data(training_data["Romney"])
+    obama_training_data = clean_data(training_data["Obama"]).sample(frac=1).reset_index()
+    romney_training_data = clean_data(training_data["Romney"]).sample(frac=1).reset_index()
 
-    # create our text vectorizer
-    text_vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=1, min_df=1, stop_words="english")
+    # create our text vectorizers
+    obama_text_vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=1, min_df=1, stop_words="english")
+    romney_text_vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=1, min_df=1, stop_words="english")
 
     # build obama and romney classifiers
-    obama_classifier = build_basic_classifier(obama_data, text_vectorizer)
+    obama_classifier = build_basic_classifier(obama_training_data, obama_text_vectorizer)
+    romney_classifier = build_basic_classifier(romney_training_data, romney_text_vectorizer)
 
+    # load test data
     test_data = load_tweet_data(opts.test)
-    obama_test = list(clean_data(test_data["Obama"])["Anotated Tweet"])
-    romney_test = clean_data(test_data["Romney"])
+    
+    # load obama test data
+    obama_test_data = clean_data(test_data["Obama"])
+    obama_tweets = list(obama_test_data["Anotated Tweet"])
+    obama_correct_classes = list(obama_test_data["Class"])
+
+    # load romney test data
+    romney_test_data = clean_data(test_data["Romney"])
+    romney_tweets = list(romney_test_data["Anotated Tweet"])
+    romney_correct_classes = list(romney_test_data["Class"])
 
     # test Obama
-    test_classifier(obama_classifier, test_data["Obama"], text_vectorizer)
-    # transformed_tweets = text_vectorizer.transform(obama_test)
-    # # transformed_obama = text_vectorizer.transform(obama_test)
-    # # print(transformed_obama)
-    # prediction = obama_classifier.predict(transformed_tweets)
-
-    # print(prediction)
-
-    # for key,test_data in test_dfs.items():
-    #     test_data["df"] = clean_data(test_data["df"])
-    #     print(test_data["df"])
-        
-
-    # load the test data after we train our model
-    # test_dfs = load_tweet_data(opts.test)
+    test_classifier(obama_classifier, obama_tweets, obama_correct_classes, obama_text_vectorizer, "Obama Classifier")
+    
+    # test Romney
+    test_classifier(romney_classifier, romney_tweets, romney_correct_classes, romney_text_vectorizer, "Romney Classifier")
